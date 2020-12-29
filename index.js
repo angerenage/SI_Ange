3dac1513290939b3affe2ea063b2b5590b81fb9a
ange ROLLET <37471613+angerenage@users.noreply.github.com>
2020-12-29 23:52:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-var app = require('express')();				//Importatoin des librairies (express pour le serveur web)
-var http = require('http').Server(app);			//Paramètrage d'express
+var app = require('express')();				//Importation de librairies (express pour le serveur web)
+var http = require('http').Server(app);
 var io = require('socket.io')(http);			//Importation de Socket.io
 
 const io_client = require("socket.io-client");		//Création du client Socket.io
-var state = "0";					//Définition de la variabke state
+var state = "0";					//Définition de l'état de la variable
 
-const socket = io_client("https://localhost/", {	//Définition des paramètres pour socket.io
+const socket = io_client("http://localhost/", {
   reconnectionDelayMax: 10000
 });
 
-app.get("/", function(req, res) {				//S'éxécute lorsque une requette est éfectué a la racine du serveur
-	res.send(state.toString());				//Repond en envoyant la valeur de la variable state
-	if (typeof req.query['message'] !== 'undefined') {	//Vérifis si le paramètre message existe
-		io.emit('M5stack', req.query['message']);	//Envoie le message de l'arduino a tout les 'abonnées'
-		console.log(req.query['message']);		//Ecrit dans la console
+app.get("/", function(req, res) {				//S'exécute lorsqu'une requette est effectuée à la racine du serveur
+	res.send(state.toString());				//Répondre en envoyant la valeur de la variable State
+	if (typeof req.query['message'] !== 'undefined') {	//Vérifiez si le paramètre message existe
+		io.emit('M5stack', req.query['message']);	//Envoyez le message de l'arduino à tous les 'abonnés'
+		console.log(req.query['message']);		//Écrit dans la console
 	}
 });
 
-io.on('connection', function(socket){				//S'éxéute lorsque un client se connecte au serveur Socket.io
-    socket.on('message', function(message){			//s'éxécute lorsque le serveur ressois un message sur la 'fréquence' message
-		console.log("message : " + message);		//Ecrit dans la console
-		if (message.split('=')[0] == "State") {		//Vérifit que la commande est valide
-			state = message.split('=')[1];		//Mis a jour de la variable state
+io.on('connection', function(socket){				//S'exécute lorsqu'un client se connecte au serveur Socket.io
+    socket.on('message', function(message){			//S'exécute lorsque le serveur reçoit une requette sur la 'fréquence' message
+		console.log("message : " + message);		//Écrit dans la console
+		if (message.split('=')[0] == "State") {		//Vérifie que la commande est valide
+			state = message.split('=')[1];		//Mise à jour de la variable State
 		}
 	});
 });
 
-http.listen(3000);	//Ecoute localhost sur le port 3000
+http.listen(3000);	//Écoute localhost sur le port 3000
